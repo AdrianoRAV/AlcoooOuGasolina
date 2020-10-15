c9d2a93c6388f94a2c37b86731ed7c537582ff4d
adriano <adrianoskte@gmail.com>
2020-10-15 16:16:12 -0300

    end project

--- app/src/main/java/com/mora/lcoolougasolina/MainActivity.java
+++ app/src/main/java/com/mora/lcoolougasolina/MainActivity.java
@@ -30,6 +30,49 @@ public class MainActivity extends AppCompatActivity {
         String precoAlcool = editPrecoAlcool.getText().toString();
         String precoGasolina = editPrecoGasolina.getText().toString();
 
+        //validar campos digitados
+
+        Boolean camposValidados = validarCampos(precoAlcool,precoGasolina);
+//        if(  ){
+//                // Nesse if colocaria a condição para o valor ser preenchido e no metodo abaixo chama ela
+//        }
+            if (camposValidados ){
+
+                //convertendo String para números
+                Double valorAlcool = Double.parseDouble(precoAlcool);
+                Double valorGasolina = Double.parseDouble(precoGasolina);
+                Double calculo;
+               calculo =  (valorAlcool/valorGasolina);
+               if (calculo >= 0.7){
+                   textoResultado.setText("Melhor usar gasolina");
+               }else{
+                   textoResultado.setText("Melhor usar Álcool");
+               }
+                /*fazer calculo de menor preço
+                *se (valorAlcool / valorGasolina) >= 0.7 é melhor utilizar o Álcool
+                 */
+
+            }else {
+                textoResultado.setText("Preencha os preços primeiro!");
+            }
+    }
+    public  Boolean validarCampos(String pAlcool, String pGasolina){
+        //public  String validarCampos(String pAlcool, String pGasolina){
+
+        Boolean camposValidadeos = true;
+      //  String campo = "preenchido";
+
+        if( pAlcool == null || pAlcool.equals("")){
+           // campo = "Àlcool";
+            camposValidadeos = false;
+        }else
+            if( pGasolina == null || pGasolina.equals("")){
+           // campo "Gasolina";
+            camposValidadeos = false;
+        }
+
+        return camposValidadeos;
+        //return campo;
     }
 
 }
